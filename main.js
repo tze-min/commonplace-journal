4069bbc37a5279ce7de0835f4af77caa76fe2bbb
tze-min <tzemin.koay@gmail.com>
2024-10-06 01:25:16 +0800

    replace themes and programmes section in homepage with dynamic list of posts

--- main.js
+++ main.js
@@ -1,34 +1,70 @@
-let tags_list = new Set();
+let POSTS_SET = new Set();
+let TAGS_SET = new Set();
+const FILEPATH = 'pages.json';
 
-async function get_posts() {
-    const filepath = 'pages.json';
-
+async function getPosts() {
+// fetch all posts from pages.json
     try {
-        const response = await fetch(filepath);
+        const response = await fetch(FILEPATH);
         if (!response.ok) {
             throw new Error(`Response status: ${response.status}`);
         }
-        const json = await response.json();
-        json.forEach(page => {
-            page.tags.forEach(tag => tags_list.add(tag));
-        });
-        console.log(json);
+        const postsList = await response.json();
+        console.log('postsList after fetching json', postsList);
+        
+        // display a unique set of tags
+        postsList.forEach(page => {
+            page.tags.forEach(tag => {
+                TAGS_SET.add(tag);
+            })
+            const content = [page.url, page.title];
+            POSTS_SET.add(content);
+        })
     } catch (error) {
         console.error(error.message);
     }
 
-    show_tags();
+    showTags();
+    showPosts();
 }
 
-function show_tags() {
-    console.log(tags_list);
+function showTags() {
+// display a unique list of tags
     const tagList = document.getElementById('tagList');
-    tags_list.forEach(tag => {
-        const tag_element = document.createElement('span');
-        tag_element.innerHTML = tag;
-        tag_element.className = 'tag';
-        tagList.appendChild(tag_element);
+    TAGS_SET.forEach(tag => {
+        const tagButton = document.createElement('button');
+        tagButton.innerHTML = tag;
+        tagButton.className = 'tagContainer';
+        // tagButton.addEventListener('click', filterPosts(tag));
+
+        tagList.appendChild(tagButton);
     })
 }
 
-get_posts();
+// function filterPosts(selectedTag) {
+//     // filter posts given a tag
+//         console.log('im getting run');
+//         const filteredPosts = POSTS_SET.filter(post => post.tags.includes(selectedTag));
+//         console.log(filteredPosts);
+//         showPosts(filteredPosts);
+//     }
+
+function showPosts(filteredPosts = POSTS_SET) {
+// display a unique list of posts, filtered if a tag's been selected
+    const postList = document.getElementById('postList');
+    filteredPosts.forEach(post => {
+        console.log('looping through', post);
+        const [url, title] = [post[0], post[1]];
+        const postContainer = document.createElement('div');
+        postContainer.className = 'postContainer';
+
+        postElement = document.createElement('a');
+        postElement.innerHTML = title;
+        postElement.href = url;
+
+        postContainer.appendChild(postElement);
+        postList.appendChild(postContainer);
+    })
+}
+
+getPosts();
