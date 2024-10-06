06ac4f786896972364f3b1d50ef6251b72e24199
tze-min <tzemin.koay@gmail.com>
2024-10-06 12:35:03 +0800

    rename generate-pages-json.py's functions

--- scripts/generate-pages-json.py
+++ scripts/generate-pages-json.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 HTML_DIRECTORY = './pages'
 PAGES_JSON = 'pages.json'
 
-def get_html_tags(filepath):
+def get_html_pages(filepath):
     '''
     Visit a html file given its filepath, parse the contents, and return a dict of the title, filepath and data-tags
     '''
@@ -22,21 +22,21 @@ def get_html_tags(filepath):
         }
         return result
 
-def get_html_tags_from_directory(directory):
+def get_html_pages_from_directory(directory):
     '''
     Given a directory of html files, iterate through each file and apply get_html_tags to return a list of dict
     '''
     pages = []
     for filename in os.listdir(directory):
         filepath = directory + '/' + filename
-        pages.append(get_html_tags(filepath))
+        pages.append(get_html_pages(filepath))
     return pages
-        
+
 def generate_pages_json(directory):
     '''
     Replace PAGES_JSON file with html tags yoinked from HTML_DIRECTORY
     '''
-    pages = get_html_tags_from_directory(directory)
+    pages = get_html_pages_from_directory(directory)
     with open(PAGES_JSON, 'w', encoding='utf-8') as outfile:
         json.dump(pages, outfile, indent=2)
 
